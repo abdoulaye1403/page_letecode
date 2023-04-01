a29bf21059d8196b8535adf764b1634895ff475c
abdoulaye <abdelraul140385@gmail.com>
2023-04-01 22:35:42 +0000

    ajout fonctionnalites

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,26 @@
 const annee = document.getElementById('annee');
+const hambuger = document.getElementById('toogle');
+const navbar = document.getElementById('navtoogle');
+const items = document.getElementById('items');
+const entete = document.getElementById('entete');
 
 const date = new Date()
 const year = date.getFullYear()
 annee.innerHTML += year 
+
+hambuger.addEventListener('click',()=>{
+  navbar.style.display='flex'
+  navbar.style.position ='absolute'
+  navbar.style.top='10px'
+  navbar.style.right='60px'
+
+  items.style.display ='flex'
+  items.style.flexDirection ='column'
+
+  entete.style.height = '180px'
+})
+
+hambuger.addEventListener('dblclick',()=>{
+    navbar.style.display='none'
+    entete.style.height = '60px'
+})
