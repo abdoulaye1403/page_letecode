26aa83662944089da401a25e6dc6c7a9440ff436
abdoulaye <abdelraul140385@gmail.com>
2023-04-01 23:08:21 +0000

    modification fonctionnalites

--- static/js/script.js
+++ static/js/script.js
@@ -9,18 +9,8 @@ const year = date.getFullYear()
 annee.innerHTML += year 
 
 hambuger.addEventListener('click',()=>{
-  navbar.style.display='flex'
-  navbar.style.position ='absolute'
-  navbar.style.top='10px'
-  navbar.style.right='60px'
-
-  items.style.display ='flex'
-  items.style.flexDirection ='column'
-
-  entete.style.height = '180px'
+  entete.classList.toggle("active");
+  navbar.classList.toggle("active");
+  items.classList.toggle("active");
 })
 
-hambuger.addEventListener('dblclick',()=>{
-    navbar.style.display='none'
-    entete.style.height = '60px'
-})
